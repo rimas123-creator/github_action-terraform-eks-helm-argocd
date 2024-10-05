4136ecf27c92a29f6abf4e5b0ec34af32220f99b
rimas123-creator <caderrimas1999@gmail.com>
2024-10-05 13:06:02 +0530

    fix: bucket name issue fixed

--- terraform-eks/backend.tf
+++ terraform-eks/backend.tf
@@ -1,9 +1,10 @@
 provider "aws" {
   region = var.region
 }
+
 terraform {
   backend "s3" {
-    bucket = "cloudwithus-terraform-state-bucket"
+    bucket = "cloudwithus-terraform-state-bucket-01"
     region = "us-east-1"
     key = "eks/terraform.tfstate"
   }
